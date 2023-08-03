19bfad46796ebfd3d63908f16854b65a6ae7468c
Luciano Franchino <luciano_franchino@hotmail.com>
2023-08-03 15:17:31 -0300

    add car behavior like aceleration, max velocity and break

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Movimiento
+    public float aceleration = 10f;
+    public float maxSpeed = 20f;
+    public float currentSpeed = 0f;
+    //public float reverse = 5f;
+    public float brakeFroce = 30f;
 
-    public float speed = 20f;
-    public float turnSpeed = 10f;
-    [SerializeField] float horizontalIn;
-    [SerializeField] float fowardIn;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Giro
+    public float turnSpeed = 80f; 
+    public float maxTurnAngle = 30f;
 
     // Update is called once per frame
     void Update()
     {
+        Movimiento();
+        transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
+        Giro();
+    }
 
-        horizontalIn = Input.GetAxis("Horizontal");
-        fowardIn = Input.GetAxis("Vertical");
+    void Movimiento()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed += aceleration * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed -= brakeFroce * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed *= 0.95f;
+        }
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardIn);
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalIn);
+        currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
+    }
+    void Giro()
+    {
+        if (currentSpeed > 0f) 
+        {
+            float turnInput = Input.GetAxis("Horizontal");
+            float turnAngle = turnInput * turnSpeed * Time.deltaTime * currentSpeed / maxSpeed;
+
+            turnAngle = Mathf.Clamp(turnAngle, -maxTurnAngle, maxTurnAngle);
+            transform.Rotate(Vector3.up, turnAngle);
+        }
     }
 }
